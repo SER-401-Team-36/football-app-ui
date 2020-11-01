db21612b4c9f9d0b4d50bb434e7b8bdee5f6c8e8
ehermick <45372760+ehermick@users.noreply.github.com>
2020-10-31 23:13:57 -0500

    US#3Task1: Create View Page

--- footballui/src/App.js
+++ footballui/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap';
 
-function App() {
+function Players() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="Players">Players</h1>
+
+        <div id="Positions">
+          <Button className="Buttons">All</Button>
+          <Button className="Buttons">QB</Button>
+          <Button className="Buttons">RB</Button>
+          <Button className="Buttons">TE</Button>
+        </div>
       </header>
+
+      <main id="ViewPage">
+          <div id="HotPicks">
+            <div id="PickHeader">
+              <h4 id="Picks">Top Hot Picks</h4>
+              <h5 id="Date">----- Date -----</h5>
+            </div>
+          </div>
+      </main>
     </div>
   );
 }
 
-export default App;
+export default Players;

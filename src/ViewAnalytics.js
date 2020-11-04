36c8e5e3b39e04955ab4789a1462bf47343c9ae2
ehermick <45372760+ehermick@users.noreply.github.com>
2020-11-03 21:41:37 -0600

    US#4Task1&2: Setup Analytics page similar to players page

--- src/ViewAnalytics.js
+++ src/ViewAnalytics.js
@@ -1,14 +1,38 @@
 import React from "react";
 import './ViewAnalytics.css';
-
+import {Button} from 'react-bootstrap';
+import { render } from 'react-dom';
 
 const ViewAnalytics = () => {
+  const currentDate=new Date().toLocaleDateString();
 
   return ( 
-    <div id="ViewAnalyticsPage">
-        <h2>View Analytics Page</h2>
-    </div>
+    <div className="App">
+      <header className="App-header">
+        <h1 id="Analytics">Analytics</h1>
 
+        <div id="Positions">
+          <Button className="Buttons">All</Button>
+          <Button className="Buttons">QB</Button>
+          <Button className="Buttons">RB</Button>
+          <Button className="Buttons">TE</Button>
+        </div>
+      </header>
+
+      <main id="ViewPage">
+
+          <div id="Analysis">
+
+          </div>
+
+          <div id="HotPicks">
+            <div id="PickHeader">
+              <h4 id="Picks">Top Hot Picks</h4>
+              <h5 id="Date">{currentDate}</h5>
+            </div>
+          </div>
+      </main>
+    </div>
   );
 }
 

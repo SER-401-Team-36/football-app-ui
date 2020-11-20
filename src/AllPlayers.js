3257222399aa75fb8cbd51ca73165c1afa3c8e1b
Renee Shull <reneekshull@gmail.com>
2020-11-19 19:35:54 -0800

    Fix modal issues

--- src/AllPlayers.js
+++ src/AllPlayers.js
@@ -7,15 +7,120 @@ import { Button } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import PlayerInfoModal from "./PlayerInfoModal.js";
+import { Bar } from "react-chartjs-2";
+
+function getModalStyle() {
+  const bottom = 10;
+  const left = 30;
+
+  return {
+    bottom: `${bottom}%`,
+    left: `${left}%`,
+    transform: `translate(-${bottom}%, -${left}%)`,
+  };
+}
+
+const state = {
+  labels: ["2018", "2019", "2020"],
+  datasets: [
+    {
+      label: "FFP",
+      backgroundColor: "rgb(110, 94, 254)",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 2,
+      data: [65, 59, 80],
+    },
+  ],
+};
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    border: "1px solid #000",
+    padding: theme.spacing(3, 3, 3),
+    width: 750,
+    height: 400,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
 function AllPlayers({ name, image, position, FFP, TD }) {
+  const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
-  // const classes = useStyles();
+  const classes = useStyles();
 
   return (
     <div className="playersInAllFeed">
-      <PlayerInfoModal />
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <p className="playerInfoInModal">
+            <Grid
+              container
+              direction="column"
+              justify="flex-end"
+              alignItems="flex-end"
+            >
+              <Grid item xs={1500}>
+                <Button
+                  className="exitButtonInModal"
+                  onClick={() => setOpen(false)}
+                >
+                  CLOSE
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justify="space-around"
+              alignItems="flex-start"
+            >
+              <Grid item xs={650}>
+                <Typography variant="body1" gutterBottom>
+                  <strong> Name: </strong> {name}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  <strong> Pos: </strong> {position}
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  <strong> FFP: </strong> {FFP}
+                </Typography>
+              </Grid>
+              <Grid item xs={650}>
+                <img className="playerImageInModal" src={image} alt="" />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              justify="flex-end"
+              alignItems="center"
+            >
+              <Grid item xs={1200}>
+                <center>
+                  <div className="canvas">
+                    <Bar
+                      data={state}
+                      options={{
+                        title: {
+                          display: true,
+                          text: "Player Stats",
+                          fontSize: 30,
+                        },
+                        legend: {
+                          display: true,
+                          position: "right",
+                        },
+                      }}
+                    />
+                  </div>
+                </center>
+              </Grid>
+            </Grid>
+          </p>
+        </div>
+      </Modal>
+
       <div>
         <Avatar className="playerAvatarInAllFeed" alt={name} src={image} />
       </div>

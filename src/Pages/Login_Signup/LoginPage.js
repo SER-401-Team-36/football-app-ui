be17270faf1a694034e22c062912f393a182a238
ehermick <45372760+ehermick@users.noreply.github.com>
2021-01-19 22:59:31 -0600

    Sem2US#1-Task1: Added Login box

--- src/Pages/Login_Signup/LoginPage.js
+++ src/Pages/Login_Signup/LoginPage.js
@@ -2,11 +2,15 @@ import './LoginPage.css';
 
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import sparky from '../../images/Sparky.jpg';
 
 const LoginPage = () => {
     return (
-        <div>
-            Login Page
+        <div id="pageHolder">
+            <div id="loginBox">
+                <img id="sparkyImg" alt="Sparky Background" src={sparky} />
+                <h1 id="login">Login</h1>
+            </div>
         </div>
     );
   };

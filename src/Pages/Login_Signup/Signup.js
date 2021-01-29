81d77998ae1e396a4fc4caf6c0d279351435a957
ehermick <45372760+ehermick@users.noreply.github.com>
2021-01-29 14:02:30 -0600

    Sem2US#2 Task1: Added background image to signup box

--- src/Pages/Login_Signup/Signup.js
+++ src/Pages/Login_Signup/Signup.js
@@ -1,11 +1,12 @@
 import './Signup.css';
 import React from 'react';
+import pitchfork from '../../images/pitchfork.jpg';
 
 const Signup = () => {
   return (
     <div id="pageHolder">
       <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
-      <div id="loginBox">
+      <div id="signupBox">
         <h1>Signup Page</h1>
       </div>
     </div>

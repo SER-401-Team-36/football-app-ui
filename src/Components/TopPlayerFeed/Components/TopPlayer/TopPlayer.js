bd6074572dbdc33392413f5208efe4a7c29ec6c5
Renee Shull <reneekshull@gmail.com>
2021-02-04 20:10:07 -0800

    Bringing code parity to Top Players (from All Players)

--- src/Components/TopPlayerFeed/Components/TopPlayer/TopPlayer.js
+++ src/Components/TopPlayerFeed/Components/TopPlayer/TopPlayer.js
@@ -3,7 +3,12 @@ import './TopPlayer.css';
 import Avatar from '@material-ui/core/Avatar';
 import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { Bar } from 'react-chartjs-2';
+import AllPlayers from '../../../AllPlayerFeed/Components/AllPlayers/AllPlayers.js';
 
 function getModalStyle() {
   const bottom = 50;
@@ -30,26 +35,195 @@ function TopPlayer({ name, image, position, FFP, TD }) {
   const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
   const classes = useStyles();
+  const [player, setPlayer] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
+
+  useEffect(() => {
+    fetch('/players').then((res) =>
+      res.json().then((data) => {
+        setPlayer(data);
+      }),
+    );
+   
+  }, []);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSearchText(event.target.value);
+  };
+
+  const handleSearchClick = async (event) => {
+    event.preventDefault();
+    const results = await fetch(
+      `/players?match_on_name=${searchText}`,
+    );
+    setPlayer(await results.json());
+  };
+
+  let chartData = {
+    labels: FFP,
+    datasets: [
+      {
+        label: name,
+        backgroundColor: 'rgb(110, 94, 254)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [25],
+      },
+    ],
+  };
+
+  let compData = {
+    labels: FFP,
+    datasets: [
+      {
+        label: name,
+        backgroundColor: 'rgb(110, 94, 254)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: [25],
+      },
+    ],
+  };
 
   return (
     <div className="topPlayers">
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <p className="topPlayers__modal">
-            <img
-              className="topPlayers__modal__img"
-              src={image}
-              alt=""
-            />
-            <strong> Name: </strong>
-            {name} <strong> Pos: </strong>
-            {position} <strong> FFP: </strong>
-            {FFP}
-          </p>
-          <p>
-            <center>
-              The graph and comparison data will be below here
-            </center>
+          <p className="playerInfoInModal">
+            <Grid
+              container
+              direction="column"
+              justify="flex-end"
+              alignItems="flex-end"
+            >
+              <Grid item xs={1500}>
+                <Button
+                  className="exitButtonInModal"
+                  onClick={() => setOpen(false)}
+                >
+                  CLOSE
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="flex-start"
+            >
+              <div id = "player_details">
+              <Grid item xs={650}>
+                <Typography variant="body1">
+                  <strong> Name: </strong> {name}
+                </Typography>
+                <Typography variant="body1">
+                  <strong> Pos: </strong> {position}
+                </Typography>
+                <Typography variant="body1">
+                  <strong> FFP: </strong> {FFP}
+                </Typography>
+              </Grid>
+              <Grid item xs={650}>
+                <img
+                  className="playerImageInModal"
+                  src={image}
+                  alt=""
+                />
+              </Grid>
+              </div>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justify="space-around"
+              alignItems="flex-start"
+            >
+              <main id="players__view">
+              <div id="players__graph">
+                  <Grid item xs={200}>
+                    <div className="canvas">
+                          <Bar
+                            data={chartData}
+                            options={{
+                              title: {
+                                display: true,
+                                text: name + ' FFP',
+                                fontSize: 20,
+                              },
+                              legend: {
+                                display: true,
+                                position: 'right',
+                              },
+                            }}
+                          />
+
+                          <Bar
+                            data={chartData}
+                            options={{
+                              title: {
+                                display: true,
+                                text: 'compName + FFP',
+                                fontSize: 20,
+                              },
+                              legend: {
+                                display: true,
+                                position: 'right',
+                              },
+                            }}
+                          />
+                      </div>
+                  </Grid>
+              </div>
+              <div className="players__comparison">
+                  <Grid item xs={1000}>
+                  <div className="compPlayerFeed">
+                    <h2 className="allPlayerFrame__title">Select Player to Compare</h2>
+                    <div className="allPlayerFrame__stickyPlayer">
+                      <h4 className="allPlayerFrame__text">
+                        <input
+                          className="allPlayerFrame__searchField"
+                          type="text"
+                          placeholder="Search.."
+                          value={searchText}
+                          onChange={handleChange}
+                        />
+                        <Button
+                          className="allPlayerFrame__searchButton"
+                          onClick={handleSearchClick}
+                        >
+                          {
+                            <img
+                              className="allPlayerFrame__searchButtonImage"
+                              alt=""
+                              src="https://img.icons8.com/pastel-glyph/2x/search--v2.png"
+                            />
+                          }
+                        </Button>
+                      </h4>
+                    </div>
+                    <div>
+                      {player &&
+                        player.map((player) => {
+                          return (
+                            <AllPlayers
+                              key={player.id}
+                              name={player.name}
+                              image={'player.image'}
+                              position={player.position}
+                              FFP={player.average_projection}
+                              TD={TD}
+                            />
+                          );
+                        })}
+                    </div>
+                  </div>
+                    
+                    </Grid>
+              </div>
+              </main>
+            </Grid>
           </p>
         </div>
       </Modal>

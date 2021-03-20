dda856f90bb3b3889265a581b9fd0e64e8ac00ef
RockslideJr <jborden36@gmail.com>
2021-03-20 14:10:38 -0700

    Add top recommended QB and WR

--- src/Pages/Draft/Components/DraftFeed/Components/RecommendedPlayers/RecommendedPlayers.js
+++ src/Pages/Draft/Components/DraftFeed/Components/RecommendedPlayers/RecommendedPlayers.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import DraftablePlayer from '../DraftablePlayers';
 
+import './RecommendedPlayer.css';
+
 const RecommendedPlayers = ({
   availablePlayers,
   onUserSelection,
   onNonUserSelection,
 }) => {
-  const topPlayer = availablePlayers.sort(
+  const sortedPlayers = availablePlayers.sort(
     (a, b) => b.average_projection - a.average_projection,
+  );
+  const topPlayer = sortedPlayers[0];
+  const topQB = sortedPlayers.filter(
+    (player) => player.position === 'QB',
+  )[0];
+  const topWR = sortedPlayers.filter(
+    (player) => player.position === 'WR',
   )[0];
 
   return (
-    <div>
-      <h3>Recommended Player: </h3>
-      {topPlayer && (
-        <DraftablePlayer
-          player={topPlayer}
-          onUserSelection={onUserSelection}
-          onNonUserSelection={onNonUserSelection}
-        />
-      )}
+    <div className="recommended_players">
+      <div className="recommended_players__recommendation">
+        <h3>Top Recommended Player: </h3>
+        {topPlayer && (
+          <DraftablePlayer
+            player={topPlayer}
+            onUserSelection={onUserSelection}
+            onNonUserSelection={onNonUserSelection}
+          />
+        )}
+      </div>
+      <div className="recommended_players__recommendation">
+        <h3>Top Recommended QB: </h3>
+        {topQB && (
+          <DraftablePlayer
+            player={topQB}
+            onUserSelection={onUserSelection}
+            onNonUserSelection={onNonUserSelection}
+          />
+        )}
+      </div>
+      <div className="recommended_players__recommendation">
+        <h3>Top Recommended WR: </h3>
+        {topWR && (
+          <DraftablePlayer
+            player={topWR}
+            onUserSelection={onUserSelection}
+            onNonUserSelection={onNonUserSelection}
+          />
+        )}
+      </div>
     </div>
   );
 };

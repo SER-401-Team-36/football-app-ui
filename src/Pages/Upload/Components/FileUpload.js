71be76d751bef074ba87ff9cf16d705e7e12c78f
csenz <casenz@hawaii.edu>
2021-04-09 08:27:03 -0700

    Added in a remove button in the upload form

--- src/Pages/Upload/Components/FileUpload.js
+++ src/Pages/Upload/Components/FileUpload.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import './FileUpload.css';
 
 const FileUpload = () => {
   return (
-    <div className="conatiner">
-      <div className="row">
-        <div className="col">
-          <form
-            action="http://localhost:5000/upload/upload"
-            method="POST"
-            encType="multipart/form-data"
-          >
-            <div className="form-group">
-              <div className="custom-file">
-                <input
-                  type="file"
-                  className="custom-file-input"
-                  name="file"
-                  id="file"
-                />
-                <label
-                  className="custom-file-label"
-                  htmlFor="custom-file"
-                ></label>
+    <div className="container">
+      <h4 className="upload__title">
+        <i className="fab fa-react">Upload File</i>
+
+        <div className="row">
+          <div className="col">
+            <form
+              action="http://localhost:5000/upload/upload"
+              method="POST"
+              encType="multipart/form-data"
+            >
+              <div className="form-group">
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    className="custom-file-input"
+                    name="file"
+                    id="file"
+                  />
+                  <label
+                    className="custom-file-label"
+                    htmlFor="custom-file"
+                  ></label>
+                </div>
               </div>
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+              <button type="submit" className="btn btn-primary">
+                Remove data
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      </h4>
     </div>
   );
 };
--- src/Pages/Upload/Upload.js
+++ src/Pages/Upload/Upload.js
@@ -5,9 +5,6 @@ import FileUpload from './Components/FileUpload';
 const Upload = () => {
   return (
     <div className="upload">
-      <h4 className="upload__title">
-        <i className="fab fa-react">Upload File</i>
-      </h4>
       <FileUpload />
     </div>
   );
